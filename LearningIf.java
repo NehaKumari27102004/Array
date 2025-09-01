048c4e3ed73080bc006e26534099f1d6aff57abd
Neha <161303798+NehaKumari27102004@users.noreply.github.com>
2025-09-02 00:25:09 +0530

    Add files via upload

---
+++ LearningIf.java
@@ -0,0 +1,15 @@
+package Arrays;
+
+public class LearningIf {
+    public static void main(String[] args){
+        boolean isMale=false;
+        String name = "Daya";
+        System.out.println("before if");
+        if(isMale){
+            System.out.println("Mr."+name);
+        }else{
+            System.out.println("Ms." +name);
+        }
+        System.out.println("after if");
+    }
+}
